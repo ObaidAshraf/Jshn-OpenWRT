bc666b727e603538d1ac6ac719fbc56cf2d75dd8
Obaid Ashraf <ubaidashraf91@gmail.com>
2017-02-14 13:50:32 +0500

    Added useful comments.

--- jshnParse3.sh
+++ jshnParse3.sh
@@ -16,10 +16,12 @@ MSG=$(cat /data2.txt)
 ## }
 ## }
 
-json_load "$MSG"
-json_get_var status status
+## json_get_var <local_var> <json_var>
+
+json_load "$MSG"    ## Load MSG string
+json_get_var status status    ## Get Value of status inside JSON string (i.e. MSG) into local "status" variable
 json_get_var msg msg
-json_select result
+json_select result            ## Select "result" object of JSON string (i.e. MSG)
 json_get_var number number
 json_get_var mac mac
 json_get_var last_seen last_seen
